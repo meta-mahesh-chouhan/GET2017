b07dde38f6ac0ae2c801e9fc30221bfeff767ed8
meta-mahesh-chouhan <mahesh.chouhan@metacube.com>
2017-08-14 04:32:42 -0700

    Update Function.js

--- JavaScriptAssignment3/Function.js
+++ JavaScriptAssignment3/Function.js
@@ -3,24 +3,53 @@
  */
 function sum(firstNumber, secondNumber) {
 	
-	if(firstNumber.trim() == "" || secondNumber.trim() == "" || isNaN(firstNumber) || isNaN(secondNumber)){
-		return "Enter Number";
-	}
-	else{
 	// If second nnumber is not passed in arguments
 	if (secondNumber === undefined) {
 		return function(thirdNumber) {
-			return parseInt(firstNumber) + parseInt(secondNumber);
+			return parseInt(firstNumber) + parseInt(thirdNumber);
 		};
 	}
 	return parseInt(firstNumber) + parseInt(secondNumber);
 	
+}
+
+/*
+ * onClick to handle sum(firstNumber,secondNumber)
+ */
+function onClickSum1(){
+	
+	if(checkEmptyFields())
+	{
+	document.getElementById("display").innerHTML = sum(document.getElementById("firstNumber").value , document.getElementById("secondNumber").value);
+	}
+	else{
+	document.getElementById("display").innerHTML = "Enter Number";
 	}
 }
 
 /*
- * onClick to handle sum
+ * onClick to handle sum(firstNumber)(secondNUmber)
  */
-function onClickSum(){
-	document.getElementById("display").innerHTML=sum(document.getElementById("firstNumber").value,document.getElementById("secondNumber").value);
+function onClickSum2(){
+	if(checkEmptyFields())
+	{
+	document.getElementById("display").innerHTML = sum(document.getElementById("firstNumber").value )( document.getElementById("secondNumber").value);
+	}
+	else{
+	document.getElementById("display").innerHTML = "Enter Number";
+	}
+}
+/*
+ * Check if the fields are empty
+ */
+function checkEmptyFields(){
+	var firstNumber = document.getElementById("firstNumber").value;
+	var secondNumber = document.getElementById("secondNumber").value;
+	
+	if(firstNumber.trim() == "" || isNaN(firstNumber) || secondNumber.trim() == "" || isNaN(secondNumber)){
+		return false;
+	}
+	
+	return true;
+	
 }
