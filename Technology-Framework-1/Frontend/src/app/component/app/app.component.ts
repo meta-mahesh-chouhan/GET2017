070b1b9da3001aa5b5e472749890bc5102f9d45d
meta-mahesh-chouhan <mahesh.chouhan@metacube.com>
2017-09-27 23:34:18 +0530

    Technology-Framework-1

---
+++ Technology-Framework-1/Frontend/src/app/component/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ProductSearchComponent } from '../product-search/product-search.component';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Shopping Cart';
+}
